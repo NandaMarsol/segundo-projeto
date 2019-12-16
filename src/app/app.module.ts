fcf7cc9c967451a7a1a07c625b8fa4560dd2e051
NandaMarsol <fernanda.almferreira@gmail.com>
2019-12-16 16:45:55 -0300

    Segundo Exemplo com Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// este é o cara que vai permitir fazer conexões para construir webservices
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { BuscaComponent } from './busca/busca.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { OutletComponent } from './outlet/outlet.component';
+import { MenuComponent } from './menu/menu.component';
+import { CorpoComponent } from './corpo/corpo.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TarefasComponent,
+    BuscaComponent,
+    TituloComponent,
+    OutletComponent,
+    MenuComponent,
+    CorpoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
